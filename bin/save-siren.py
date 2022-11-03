5b7cadcc3cbb2ab48b536d654b6efcdd5dfa5ca7
kvtsang <kvtsang@gmail.com>
2022-11-03 15:24:45 -0700

    [new] save light yield

--- bin/save-siren.py
+++ bin/save-siren.py
@@ -8,7 +8,7 @@ import fire
 from functools import partial
 from tqdm import trange
 
-from siren import Siren
+from siren.utils import import_from
 from photonlib import PhotonLib
 
 
@@ -16,7 +16,8 @@ def save(cfg_file, ckpt_file, outfile='siren_pred.h5', device=0):
     with open(cfg_file, 'r') as f:
         cfg = yaml.safe_load(f)
 
-    model = Siren.load_from_checkpoint(ckpt_file, strict=False, cfg=cfg)
+    Model = import_from(cfg['class']['model'])
+    model = Model.load_from_checkpoint(ckpt_file, strict=False, cfg=cfg)
     model.eval()
     model.to(device)
 
@@ -33,11 +34,19 @@ def save(cfg_file, ckpt_file, outfile='siren_pred.h5', device=0):
     for ix in trange(nx):
         torch.cuda.empty_cache()
         x = meta.idx_to_coord(meta.idx_at('x', ix, device=device), norm=True) 
-        pred, coord = model(x)
+
+        if hasattr(model, 'get_siren_output'):
+            pred, coord = model.get_siren_output(x)
+        else:
+            pred, coord = model(x)
 
         vis_pred[ix] = to_vis(pred).reshape(ny, nz, n_pmts).detach()
 
-    PhotonLib.save(outfile, vis_pred.to('cpu'), meta)
+    light_yield = getattr(model, 'light_yield', None)
+    if light_yield is not None:
+        light_yield = light_yield.detach().cpu().numpy()
+
+    PhotonLib.save(outfile, vis_pred.to('cpu'), meta, eff=light_yield)
 
 if __name__ == '__main__':
     fire.Fire(save)
